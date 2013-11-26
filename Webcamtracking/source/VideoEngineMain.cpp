21a5af265d0a09d5ba417bf70a2c9e8cc79d0317
Paco Ojka <patricksojka@gmx.net>
2013-11-26 15:17:59 +0100

    first commit

---
+++ Webcamtracking/source/VideoEngineMain.cpp
@@ -0,0 +1,12 @@
+#include "BackgroundSubstraction.h"
+using namespace cv;
+using namespace std;
+
+int main(){
+	const string videoPath = "C:\\Users\\paco\\Downloads\\Micro-dance.wmv";
+	//const string videoPath = "c:/Entwicklung/micro-dance.mpeg";
+	Backgroundsubstraction app;
+	if (app.open(videoPath)){
+		app.run();
+	}
+}
