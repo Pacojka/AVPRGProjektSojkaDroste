d2bda68ade2901a273b9a6340477accb9baa639c
troishomme <johannes.droste@haw-hamburg.de>
2014-01-10 15:26:06 +0100

    verbesserung/änderung/kürzung von DrawOnFace.cpp

--- Webcamtracking/source/DrawOnFace.cpp
+++ Webcamtracking/source/DrawOnFace.cpp
@@ -30,15 +30,18 @@ void overlayImage(Mat &background, Mat &overlay)
 }
 
 void DrawOnFace::drawOnFace(cv::Mat& resultFrame, cv::Rect& faceRect, cv::Rect& leftEyeRect, cv::Rect& rightEyeRect){
-	//Schwarzes Rechteck zu Debugzwecken zeichnen
-	    
-		//rectangle(resultFrame, Rect(leftEyeRect.x,leftEyeRect.y, rightEyeRect.x+rightEyeRect.width-leftEyeRect.x, rightEyeRect.y+rightEyeRect.height-leftEyeRect.y), Scalar(0,0,0));
+
 
 	//Mat für Brille initialisieren (inkl. Alphakanal)
 		Mat glasses = imread("brille.png", -1);
+				
+	//Brillengröße=(Brillenabstand/Augenabstand) errechnen
+		float scaleFactor = ((float) glasses.cols / ((rightEyeRect.x+rightEyeRect.width) - leftEyeRect.x));
+
+	//Brillengröße dem Skalierungsfaktor entsprechend verändern
+		resize(glasses, glasses, Size(), (1.f/scaleFactor), (1.f/scaleFactor),INTER_LINEAR);
 	
-	//Rotationswinkel des Gesichtes/der Augen berechnen
-		//Ankathete
+	//Rotationswinkel des Gesichtes/der Augen berechnen (Dreieck aufspannen)
 		Point2f a = Point2f((leftEyeRect.x+leftEyeRect.size().width/2.f),(leftEyeRect.y+leftEyeRect.size().height/2.f));
 		Point2f b = Point2f((rightEyeRect.x+rightEyeRect.size().width/2.f),(rightEyeRect.y+rightEyeRect.size().height/2.f));
 		Point2f c = Point2f(b.x,a.y);
@@ -46,59 +49,37 @@ void DrawOnFace::drawOnFace(cv::Mat& resultFrame, cv::Rect& faceRect, cv::Rect& 
 		double aside = (b.x-a.x);
 		double bside = (a.y-b.y);
 		double cside = sqrt(aside*aside+bside*bside);
-		double rota = -asin(bside/cside)*180/3.1415f;
+		double rota = -asin(bside/cside)*180/3.1415f;		
 
-		Mat faceRect_temp; 
-		resultFrame(faceRect).copyTo(faceRect_temp);
+	//Temporäres Material zum kopieren der Brille erstellen
+		Mat faceRect_temp(faceRect.size(),glasses.type(),Scalar(0,0,0,0)); 
+
+
 		namedWindow("GET REKT");
 		
+	//Rotationspunkt für Rotation der Brille (respektive des temporären Mats) erstellen
 		Point2f rotPt(((a.x+b.x)/2.f)-faceRect.x,((a.y+b.y)/2.f)-faceRect.y);
-		Mat rotMat = getRotationMatrix2D(rotPt, rota, 1.0);
-		warpAffine(faceRect_temp, faceRect_temp, rotMat, faceRect_temp.size());
 		
-
-		
-
-	//Brillengröße=(Brillenabstand/Augenabstand)
-		float scaleFactor = ((float) glasses.cols / ((rightEyeRect.x+rightEyeRect.width) - leftEyeRect.x));
-
-	//Brillengröße dem Skalierungsfaktor entsprechend verändern
-		resize(glasses, glasses, Size(), (1.f/scaleFactor), (1.f/scaleFactor),INTER_LINEAR);
-
-
-		Point2f roiPt((((a.x+b.x)/2.f)-faceRect.x)-glasses.size().width/2.f,((a.y+b.y)/2.f)-faceRect.y-glasses.size().height/2.f);
-		Mat glassesROI_temp = faceRect_temp(Rect(roiPt, glasses.size()));
+	//Region of Interest zum kopieren der Brille ins temporäre Mat erstellen
+		Mat glassesROI_temp = faceRect_temp(Rect(Point2f((((a.x+b.x)/2.f)-faceRect.x)-glasses.size().width/2.f,((a.y+b.y)/2.f)-faceRect.y-glasses.size().height/2.f), glasses.size()));
+		std::cout << "Out of Bounds 1" << std::endl;
+	//Brille per overlayImage mit Alphakanal in diese ROI kopieren
 		overlayImage(glassesROI_temp, glasses);
+
 		imshow("GET REKT", faceRect_temp);
-		rotMat = getRotationMatrix2D(rotPt, -rota, 1.0);
+
+	//Rotationsmatrix für Rotation der Brille (respektive des temporären Mats) erstellen/berechnen
+		Mat rotMat = getRotationMatrix2D(rotPt, -rota, 1.0);
+	//Der Rotationsmatrix entsprechend rotieren
 		warpAffine(faceRect_temp, faceRect_temp, rotMat, faceRect_temp.size());
-
-		std::cout << "hehejo " << std::endl;
-
-		float glassDiag = sqrtf(glasses.size().width*glasses.size().width+glasses.size().height*glasses.size().height);
-
-		glassesROI_temp = faceRect_temp(
-			Rect(Point2f(rotPt.x-glassDiag/2.f,rotPt.y-glasses.size().height/2.f-abs(bside)),
-			Size(glassDiag,
-			glasses.size().height+(abs(bside)*2))));
+	//Das temporäre Mat per overlayImage auf das resultFrame kopieren
+		overlayImage(resultFrame(faceRect), faceRect_temp);
+		std::cout << "Out of Bounds 2" << std::endl;
 		
-		/*namedWindow("AUSGABE");
-		circle(resultFrame, a, 3, Scalar(255,255,255), 1);
-		circle(resultFrame, b, 3, Scalar(255,255,255), 1);
-		circle(resultFrame, c, 3, Scalar(255,255,255), 1);
-		imshow("AUSGABE", glassesROI_temp);*/
 		
-
-		rotPt = Point(rotPt.x+faceRect.x,rotPt.y+faceRect.y);
-		Mat glassesROI = resultFrame(Rect(Point2f(rotPt.x-glassDiag/2.f,rotPt.y-glasses.size().height/2.f-abs(bside)),
-			Size(glassDiag,
-			glasses.size().height+(abs(bside)*2))));
-
-		glassesROI_temp.copyTo(glassesROI);
-
 	//HUT
 	//Mat für Hut initialisieren (inkl. Alphakanal)
-		Mat hat = imread("hut.png", -1);
+		/*Mat hat = imread("hut.png", -1);
 
 		
 		resize(hat, hat, Size(), 4.f/scaleFactor, 4.f/scaleFactor, INTER_LINEAR);
@@ -111,7 +92,7 @@ void DrawOnFace::drawOnFace(cv::Mat& resultFrame, cv::Rect& faceRect, cv::Rect& 
 		//Mat hatROI = resultFrame(Rect(faceRect.x+50,max(faceRect.y-100,0),hat.size().width,hatHeight));
 		Mat hatROI = resultFrame(Rect(leftEyeRect.x,max((leftEyeRect.y-((rightEyeRect.x+rightEyeRect.width) - leftEyeRect.x))-(hat.size().height/3),0),hat.size().width,hatHeight));
 
-		overlayImage(hatROI, hat);
+		overlayImage(hatROI, hat);*/
 				
 		
 }
