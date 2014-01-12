3261259cd3be02ce07d2df0141223ece5a384636
troishomme <johannes.droste@haw-hamburg.de>
2014-01-12 16:12:52 +0100

    Bilder und entsprechende Werte für DrawEnvironment.cpp hinzugefügt

--- Webcamtracking/source/DrawEnvironment.cpp
+++ Webcamtracking/source/DrawEnvironment.cpp
@@ -9,9 +9,9 @@ DrawEnvironment::~DrawEnvironment(){}
 
 void DrawEnvironment::initialize(int width, int height){
 	environment1 = imread("env_TV.png", -1);
-	environment2 = imread("env_TV.png", -1);
-	environment3 = imread("env_TV.png", -1);
-	environment4 = imread("env_TV.png", -1);
+	environment2 = imread("env_News.png", -1);
+	environment3 = imread("env_Cinema.png", -1);
+	environment4 = imread("env_Frame.png", -1);
 	environmentWidth = environment1.size().width;
 	environmentHeight = environment1.size().height;
 	resize(environment1, environment1, Size(width, height));
@@ -20,14 +20,14 @@ void DrawEnvironment::initialize(int width, int height){
 	resize(environment4, environment4, Size(width, height));
 	environmentScaleHeight = (float) environment1.size().height/environmentHeight;
 	environmentScaleWidth = (float) environment1.size().width/environmentWidth;
-	env1_ROI = Rect(Point(70*environmentScaleWidth,130*environmentScaleHeight),Size(913*environmentScaleWidth,669*environmentScaleHeight));
-	env2_ROI = Rect(Point(70*environmentScaleWidth,130*environmentScaleHeight),Size(913*environmentScaleWidth,669*environmentScaleHeight));
-	env3_ROI = Rect(Point(70*environmentScaleWidth,130*environmentScaleHeight),Size(913*environmentScaleWidth,669*environmentScaleHeight));
-	env4_ROI = Rect(Point(70*environmentScaleWidth,130*environmentScaleHeight),Size(913*environmentScaleWidth,669*environmentScaleHeight));
-	env1_size = Size(913*environmentScaleWidth,669*environmentScaleHeight);
-	env2_size = Size(913*environmentScaleWidth,669*environmentScaleHeight);
-	env3_size = Size(913*environmentScaleWidth,669*environmentScaleHeight);
-	env4_size = Size(913*environmentScaleWidth,669*environmentScaleHeight);
+	env1_size = Size(800*environmentScaleWidth,600*environmentScaleHeight);
+	env2_size = Size(641*environmentScaleWidth,481*environmentScaleHeight);
+	env3_size = Size(800*environmentScaleWidth,600*environmentScaleHeight);
+	env4_size = Size(958*environmentScaleWidth,668*environmentScaleHeight);
+	env1_ROI = Rect(Point(80*environmentScaleWidth,130*environmentScaleHeight),env1_size);
+	env2_ROI = Rect(Point(47*environmentScaleWidth,150*environmentScaleHeight),env2_size);
+	env3_ROI = Rect(Point(200*environmentScaleWidth,113*environmentScaleHeight),env3_size);
+	env4_ROI = Rect(Point(122*environmentScaleWidth,114*environmentScaleHeight),env4_size);
 }
 
 
